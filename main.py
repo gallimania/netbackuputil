3ed9f27d2b16ca68ee00bb80c810e9cba712ba3c
Garrett F <38448592+gallimania@users.noreply.github.com>
2018-07-22 08:41:41 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Copyright (C) 2018 Garrett Fifer
+#
+# Main python file responsible for calling program-specific functions.
+#
+# This software is licensed under the MIT license.
+#
+# Before attempting to use this Python package please ensure your system meets the following requirements:
+# - Python 3
+# - socket
+# - pycdlib
+#
+# Begin main.py
+
+include networking as netwrk
+include diskimager as dskimg
